6af73c841201f8e79bc9244e936ae2bed41dd14b
Nicolas Galvan <nicosg93@hotmail.com>
2019-05-26 18:30:56 -0300

    Agrego logica en Prenda
    Agrego en la capa de negocio, la logica de prenda, si 2 colores son iguales

---
+++ Negocio/IServicios/IPrenda.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Negocio.Enums;
+
+namespace Negocio
+{
+    public interface IPrenda
+    {
+        bool coloresValidos(EnumColor colorPrimario, EnumColor colorSecundario);
+    }
+}
---
+++ Negocio/Servicios/Prenda.cs
@@ -0,0 +1,22 @@
+﻿using Datos.Entidades;
+using Datos.Repositorios;
+using System.Collections.Generic;
+using Negocio.Enums;
+
+namespace Negocio
+{
+    public class Prenda : IPrenda
+    {
+        readonly IPrendasRepositorio _prendasRepositorio;
+
+        public Prenda(IPrendasRepositorio prendasRepositorio)
+        {
+            _prendasRepositorio = prendasRepositorio;
+        }
+
+        public bool coloresValidos(EnumColor colorPrimario, EnumColor colorSecundario)
+        {
+            return colorPrimario != colorSecundario;
+        }
+    }
+}
