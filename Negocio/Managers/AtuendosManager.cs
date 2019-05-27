f2776b8a45c8d0d402b434a0062e4fbe926b7377
esvazquez <esvazquez@TEC24M>
2019-05-27 18:54:28 -0300

    Se agrega logica para combinaciones de atuendos validas.

---
+++ Negocio/Managers/AtuendosManager.cs
@@ -0,0 +1,40 @@
+﻿using Combinatorics.Collections;
+using QueMePongo.Dominio.DTOs;
+using QueMePongo.Dominio.Interfaces;
+using QueMePongo.Dominio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QueMePongo.Negocio.Managers
+{
+    public class AtuendosManager
+    {
+        readonly IGuardarropaRepositorio _guardarropaRepositorio;
+
+        public AtuendosManager(IGuardarropaRepositorio guardarropaRepositorio)
+        {
+            _guardarropaRepositorio = guardarropaRepositorio;
+        }
+
+        public IEnumerable<Atuendo> GenerarAtuendosPorGuardarropa(int guardarropaId)
+        {
+            var guardarropa = _guardarropaRepositorio.ObtenerGuardarropaPorId(guardarropaId);
+            var combinatoria = new Combinations<Prenda>(guardarropa.Prendas.ToList(), 5);
+            foreach (var prendas in combinatoria)
+            {
+                ValidarPrendas(prendas);
+                yield return new Atuendo { Prendas = prendas };
+            }
+        }
+
+        private void ValidarPrendas(IList<Prenda> prendas)
+        {
+            var prendaValida = prendas
+                .GroupBy(p => p.Categoria).Count() == 5;
+
+            if (!prendaValida)
+                throw new Exception("Combinacion de prendas no validas");
+        }
+    }
+}
