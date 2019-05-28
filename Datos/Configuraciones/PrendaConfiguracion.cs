478c7a5516c5d4640bf467388defea09b3249a0b
esvazquez <esvazquez@TEC24M>
2019-05-28 01:26:52 -0300

    Se agrgan conversiones.

--- Datos/Configuraciones/PrendaConfiguracion.cs
+++ Datos/Configuraciones/PrendaConfiguracion.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QueMePongo.Dominio.Models;
 
 namespace QueMePongo.AccesoDatos.Configuraciones
@@ -8,7 +9,16 @@ namespace QueMePongo.AccesoDatos.Configuraciones
     {
         public void Configure(EntityTypeBuilder<Prenda> builder)
         {
+            var converterColor = new EnumToNumberConverter<Color, int>();
+            var converterCategoria = new EnumToNumberConverter<Categoria, int>();
+            var converterTela = new EnumToNumberConverter<Tela, int>();
+            var converterTipo = new EnumToNumberConverter<Tipo, int>();
 
+            builder.Property(b => b.Tipo).HasConversion(converterTipo);
+            builder.Property(b => b.Tela).HasConversion(converterTela);
+            builder.Property(b => b.Categoria).HasConversion(converterCategoria);
+            builder.Property(b => b.ColorPrimario).HasConversion(converterColor);
+            builder.Property(b => b.ColorSecundario).HasConversion(converterColor);
         }
     }
 }
