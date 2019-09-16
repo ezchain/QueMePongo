b6952f7ca05577dee9015e2248c9b7b5f6c8616e
Farid Ahmed <Farid.Ahmed@ar.ey.com>
2019-09-16 12:32:36 -0300

    Add tables

--- QueMePongo.AccesoDatos/Data/QueMePongoDbContext.cs
+++ QueMePongo.AccesoDatos/Data/QueMePongoDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using QueMePongo.AccesoDatos.Configuraciones;
+using QueMePongo.AccesoDatos.Entities;
 using QueMePongo.Dominio.Models;
 
 namespace QueMePongo.AccesoDatos.Data
@@ -12,10 +13,19 @@ namespace QueMePongo.AccesoDatos.Data
 
         }
 
-        public DbSet<Prenda> Prendas { get; set; }
-        public DbSet<Guardarropa> Guardarropas { get; set; }
-        public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<Evento> Eventos { get; set; }
+        public DbSet<PrendaEntity> Prendas { get; set; }
+        public DbSet<GuardarropaEntity> Guardarropas { get; set; }
+        public DbSet<UsuarioEntity> Usuarios { get; set; }
+        public DbSet<EventoEntity> Eventos { get; set; }
+        public DbSet<CategoriaEntity> Categorias { get; set; }
+        public DbSet<ColorEntity> Colores { get; set; }
+        public DbSet<FormalidadEntity> Formalidades { get; set; }
+        public DbSet<FrecuenciaEventoEntity> Frecuencias { get; set; }
+        public DbSet<SensibilidadEntity> Sensibilidades { get; set; }
+        public DbSet<TelaEntity> Telas { get; set; }
+        public DbSet<TipoDePrendaEntity> TiposDePrendas { get; set; }
+        public DbSet<TipoUsuarioEntity> TiposDeUsuarios { get; set; }
+        public DbSet<UbicacionEntity> Ubicaciones { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
